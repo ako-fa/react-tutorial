7aeab65801380c04b073f40092f30b51fe7edffc
ako-fa <shotaro.ako@frontarc.co.jp>
2022-04-15 07:31:36 +0900

    [update] コンポーネントの分割

--- src/App.js
+++ src/App.js
@@ -1,46 +1,98 @@
+/**
+ * ヘッダー
+ * @returns {JSX.Element}
+ */
+function Header() {
+  return (
+    <header className="hero is-dark is-bold">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">Cute Dog Image</h1>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+/**
+ * 画像
+ * @returns {JSX.Element}
+ */
+function Image() {
+  return (
+    <div className="card">
+      <div className="card-image">
+        <figure className="image">
+          <img
+            src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
+            alt="cute dog"
+          />
+        </figure>
+      </div>
+    </div>
+  );
+}
+
+/**
+ * ギャラリー
+ * @returns {JSX.Element}
+ */
+function Gallery() {
+  return (
+    <div className="columns is-vcenterd is-multiline">
+      <div className="column is-3">
+        <Image />
+      </div>
+    </div>
+  );
+}
+
+/**
+ * メインコンテンツ
+ * @returns {JSX.Element}
+ */
+function Main() {
+  return (
+    <main>
+      <section className="section">
+        <div className="container">
+          <Gallery />
+        </div>
+      </section>
+    </main>
+  );
+}
+
+/**
+ * フッター
+ * @returns {JSX.Element}
+ */
+function Footer() {
+  return (
+    <footer className="footer">
+      <div className="content has-text-centered">
+        <p>Dog images are retrieved from Dog API</p>
+        <p>
+          <a href="https://dog.ceo/dog-api/about">Donate to Dog API</a>
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+/**
+ *
+ * @returns {JSX.Element}
+ */
 function App() {
   return (
     <div>
       {/* ヘッダー */}
-      <header className="hero is-dark is-bold">
-        <div className="hero-body">
-          <div className="container">
-            <h1 className="title">Cute Dog Image</h1>
-          </div>
-        </div>
-      </header>
-
+      <Header />
       {/* メインコンテンツ */}
-      <main>
-        <section className="section">
-          <div className="container">
-            <div className="columns is-vcenterd is-multiline">
-              <div className="column is-3">
-                <div className="card">
-                  <div className="card-image">
-                    <figure className="image">
-                      <img
-                        src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
-                        alt="cute dog"
-                      />
-                    </figure>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
-      </main>
-
+      <Main />
       {/* フッター */}
-      <footer className="footer">
-        <div className="content has-text-centered">
-          <p>Dog images are retrieved from Dog API</p>
-          <p>
-            <a href="https://dog.ceo/dog-api/about">Donate to Dog API</a>
-          </p>
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 }
