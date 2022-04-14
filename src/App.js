f463106b82a9512d0ef933c89a573505bd24156f
ako-fa <shotaro.ako@frontarc.co.jp>
2022-04-15 07:19:23 +0900

    [update] bulmaのクラスを追加

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-function App () {
-    return (
-        <div>
-            <h1>Hello, World!</h1>
+function App() {
+  return (
+    <div>
+      {/* ヘッダー */}
+      <header className="hero is-dark is-bold">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">Cute Dog Image</h1>
+          </div>
         </div>
-    )
+      </header>
+
+      {/* メインコンテンツ */}
+      <main>
+        <section className="section">
+          <div className="container">
+            <div className="columns is-vcenterd is-multiline">
+              <div className="column is-3">
+                <div className="card">
+                  <div className="card-image">
+                    <figure className="image">
+                      <img
+                        src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
+                        alt="cute dog"
+                      />
+                    </figure>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+
+      {/* フッター */}
+      <footer className="footer">
+        <div className="content has-text-centered">
+          <p>Dog images are retrieved from Dog API</p>
+          <p>
+            <a href="https://dog.ceo/dog-api/about">Donate to Dog API</a>
+          </p>
+        </div>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
