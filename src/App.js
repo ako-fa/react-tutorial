136f252e515749b1677c3818d15970e53fcfc18c
ako-fa <shotaro.ako@frontarc.co.jp>
2022-04-15 13:16:37 +0900

    [update] 条件分岐によるローディング機能の追加

--- src/App.js
+++ src/App.js
@@ -30,12 +30,19 @@ function Image(props) {
   );
 }
 
+function Loading() {
+  return <p>Loading...</p>;
+}
+
 /**
  * ギャラリー
  * @returns {JSX.Element}
  */
 function Gallery(props) {
   const { urls } = props;
+  if (urls === null) {
+    return <Loading />;
+  }
   return (
     <div className="columns is-vcenterd is-multiline">
       {urls.map((url) => {
@@ -54,20 +61,22 @@ function Gallery(props) {
  * @returns {JSX.Element}
  */
 function Main() {
-  const urls = [
-    "https://images.dog.ceo/breeds/shiba/shiba-11.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-12.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-14.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-17.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-2.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-3i.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-4.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-5.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-6.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-7.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-8.jpg",
-    "https://images.dog.ceo/breeds/shiba/shiba-9.jpg",
-  ];
+  // const urls = [
+  //   "https://images.dog.ceo/breeds/shiba/shiba-11.jpg",
+  //   "https://images.dog.ceo/breeds/shiba/shiba-12.jpg",
+  //   "https://images.dog.ceo/breeds/shiba/shiba-14.jpg",
+  //   "https://images.dog.ceo/breeds/shiba/shiba-17.jpg",
+  //   "https://images.dog.ceo/breeds/shiba/shiba-2.jpg",
+  //   "https://images.dog.ceo/breeds/shiba/shiba-3i.jpg",
+  //   "https://images.dog.ceo/breeds/shiba/shiba-4.jpg",
+  //   "https://images.dog.ceo/breeds/shiba/shiba-5.jpg",
+  //   "https://images.dog.ceo/breeds/shiba/shiba-6.jpg",
+  //   "https://images.dog.ceo/breeds/shiba/shiba-7.jpg",
+  //   "https://images.dog.ceo/breeds/shiba/shiba-8.jpg",
+  //   "https://images.dog.ceo/breeds/shiba/shiba-9.jpg",
+  // ];
+
+  const urls = null;
 
   return (
     <main>
