1634290346b1e4ade4b479c108f09b3c3fd7ced4
ako-fa <shotaro.ako@frontarc.co.jp>
2022-04-16 12:30:36 +0900

    [delete] ファイルの削除

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import "bulma/css/bulma.css";
-
-import { createRoot } from "react-dom/client";
-import App from "./App";
-
-const container = document.querySelector("#content");
-const content = createRoot(container);
-content.render(<App />);
